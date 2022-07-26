01a78cc9214027fc8fdffc904f2560d0431bf043
acbrekus <acbrekus@gmail.com>
2022-07-26 03:21:22 -0400

    Fixed 'And' operator

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const computerChoices = ['Rock', 'Paper', 'Scissors'];
 function playRound(playerSelection, computerSelection) {
     if (playerSelection = computerSelection) {
         return "It's a draw!";
-    } else if ((playerSelection === 'rock' and computerSelection === 'scissors') ||
-            (playerSelection === 'scissors' and computerSelection === 'paper') ||
-            (playerSelection === 'paper' and computerSelection === 'rock')) {
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+            (playerSelection === 'scissors' && computerSelection === 'paper') ||
+            (playerSelection === 'paper' && computerSelection === 'rock')) {
             return `You won! ${playerSelection} beats ${computerSelection}.` 
     } else {
         return `You lost! ${computerSelection} beats ${playerSelection}.`
