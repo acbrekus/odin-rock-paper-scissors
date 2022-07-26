d3dbed1eeafc15707a2d75cada71fe4b8209e13c
acbrekus <acbrekus@gmail.com>
2022-07-26 03:19:33 -0400

    Added conditionals for game outcomes.

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function getComputerChoice(arr) {
- return arr[(Math.floor(Math.random()*arr.length))];
+    return arr[(Math.floor(Math.random()*arr.length))];
 }
 
 const computerChoices = ['Rock', 'Paper', 'Scissors'];
-console.log(getComputerChoice(computerChoices));
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection = computerSelection) {
+        return "It's a draw!";
+    } else if ((playerSelection === 'rock' and computerSelection === 'scissors') ||
+            (playerSelection === 'scissors' and computerSelection === 'paper') ||
+            (playerSelection === 'paper' and computerSelection === 'rock')) {
+            return `You won! ${playerSelection} beats ${computerSelection}.` 
+    } else {
+        return `You lost! ${computerSelection} beats ${playerSelection}.`
+    }
+    
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice(computerChoices);
