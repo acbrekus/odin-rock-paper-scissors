c986cacd62fecdc2486f9a302787efc5e5475a14
acbrekus <acbrekus@gmail.com>
2022-07-27 18:50:29 -0400

    Added announcements to game outcome.

--- script.js
+++ script.js
@@ -8,16 +8,15 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = prompt("Rock paper scissors shoot!");
     computerSelection = getComputerChoice(computerChoices);
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return "It's a draw!";
+        console.log("It's a draw!");
     } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') ||
             (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') ||
             (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock')) {
                 playerScore += 1;
-                return `You won! ${playerSelection} beats ${computerSelection}.`;
-            
+                console.log(`You won! ${playerSelection} beats ${computerSelection}.`);           
     } else {
         computerScore += 1;
-        return `You lost! ${computerSelection} beats ${playerSelection}.`;
+        console.log(`You lost! ${computerSelection} beats ${playerSelection}.`);
         
     }
 }
@@ -35,5 +34,14 @@ function game() {
     for (i=0; i < 5; i++) {
         playRound(playerSelection, computerSelection);
         console.log('The score is ', playerScore, ' to ', computerScore);
+        if (i===4) {
+            if (playerScore === computerScore) {
+                console.log("The game is tied!");
+            } else if (playerScore > computerScore) {
+                console.log("You won the game!");
+            } else {
+                console.log("You lost the game!");
+            }
+        }
     }
 }
