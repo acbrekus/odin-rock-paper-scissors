48637599e66a7c696afc9a0dcf8052d9d9a8bee2
acbrekus <acbrekus@gmail.com>
2022-07-27 18:42:15 -0400

    Added loop to play 5 rounds.

--- script.js
+++ script.js
@@ -5,27 +5,35 @@ function getComputerChoice(arr) {
 const computerChoices = ['Rock', 'Paper', 'Scissors'];
 
 function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Rock paper scissors shoot!");
+    computerSelection = getComputerChoice(computerChoices);
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         return "It's a draw!";
     } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') ||
             (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') ||
             (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock')) {
-            return `You won! ${playerSelection} beats ${computerSelection}.`;
-            playerScore =+ 1;
+                playerScore += 1;
+                return `You won! ${playerSelection} beats ${computerSelection}.`;
+            
     } else {
+        computerScore += 1;
         return `You lost! ${computerSelection} beats ${playerSelection}.`;
-        computerScore =+ 1;
-    }
-}
-
-const playerSelection = "scissors";
-const computerSelection = getComputerChoice(computerChoices);
-
-function game() { 
-    for (let i=0; i <=4; i++) {
         
     }
 }
 
-const playerScore = 0;
-const computerScore = 0;
+
+
+let playerSelection = "Rock";
+let computerSelection = getComputerChoice(computerChoices);
+
+
+let playerScore = 0;
+let computerScore = 0;
+
+function game() {
+    for (i=0; i < 5; i++) {
+        playRound(playerSelection, computerSelection);
+        console.log('The score is ', playerScore, ' to ', computerScore);
+    }
+}
